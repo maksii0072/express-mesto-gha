bf28b85f108e53c0db2fd1a7412483be6fba1b7f
Maksim <dolya.m.mos.av@gmail.com>
2023-07-02 15:01:49 +0300

    fix problem eslint

--- controllers/users.js
+++ controllers/users.js
@@ -1,104 +1,112 @@
 const mongoose = require('mongoose');
 const User = require('../models/user');
 const {
-    ERROR_BAD_DATA,
-    ERROR_NOT_FOUND,
-    ERROR_DEFAULT
+  ERROR_BAD_DATA,
+  ERROR_NOT_FOUND,
+  ERROR_DEFAULT,
 } = require('../utils/errors');
+
 const userDataUpdate = (req, res, updateData) => {
-    User.findByIdAndUpdate(
-            req.user._id,
-            updateData, {
-                new: true,
-                runValidators: true
+  User.findByIdAndUpdate(
+    req.user._id,
+    updateData,
+    {
+      new: true,
+      runValidators: true,
 
-            },
-        )
-        .then((user) => {
-            if (user) res.send({
-                data: user
-            });
-            else res.status(ERROR_NOT_FOUND).send({
-                message: 'Пользователь не найден'
-            });
-        })
-        .catch((err) => {
-            // eslint-disable-next-line max-len
-
-            if (err instanceof mongoose.Error.ValidationError) {
-                res.status(ERROR_BAD_DATA).send({
-                    message: 'Переданы некорректные данные пользователя.'
-                });
-            } else {
-                res.status(ERROR_DEFAULT).send({
-                    message: 'Произошла ошибка'
-                });
-            }
+    },
+  )
+    .then((user) => {
+      if (user) {
+        res.send({
+          data: user,
         });
+      } else {
+        res.status(ERROR_NOT_FOUND).send({
+          message: 'Пользователь не найден',
+        });
+      }
+    })
+    .catch((err) => {
+      // eslint-disable-next-line max-len
+
+      if (err instanceof mongoose.Error.ValidationError) {
+        res.status(ERROR_BAD_DATA).send({
+          message: 'Переданы некорректные данные пользователя.',
+        });
+      } else {
+        res.status(ERROR_DEFAULT).send({
+          message: 'Произошла ошибка',
+        });
+      }
+    });
 };
 module.exports.getAllUsers = (req, res) => {
-    User.find({})
-        .then((users) => res.send({
-            data: users
-        }))
-        .catch(() => res.status(ERROR_DEFAULT).send({
-            message: 'Произошла ошибка'
-        }));
+  User.find({})
+    .then((users) => res.send({
+      data: users,
+    }))
+    .catch(() => res.status(ERROR_DEFAULT).send({
+      message: 'Произошла ошибка',
+    }));
 };
 module.exports.getUserById = (req, res) => {
-    User.findById(req.params.userId)
-        .then((user) => {
-            if (user) res.send({
-                data: user
-            });
-            else res.status(ERROR_NOT_FOUND).send({
-                message: 'Пользователь не найден'
-            });
-        })
-        .catch((err) => {
-            if (err instanceof mongoose.Error.CastError) {
-                res.status(ERROR_BAD_DATA).send({
-                    message: 'Переданы некорректные данные пользователя.'
-                });
-            } else {
-                res.status(ERROR_DEFAULT).send({
-                    message: 'Произошла ошибка'
-                });
-            }
+  User.findById(req.params.userId)
+    .then((user) => {
+      if (user) {
+        res.send({
+          data: user,
         });
+      } else {
+        res.status(ERROR_NOT_FOUND).send({
+          message: 'Пользователь не найден',
+        });
+      }
+    })
+    .catch((err) => {
+      if (err instanceof mongoose.Error.CastError) {
+        res.status(ERROR_BAD_DATA).send({
+          message: 'Переданы некорректные данные пользователя.',
+        });
+      } else {
+        res.status(ERROR_DEFAULT).send({
+          message: 'Произошла ошибка',
+        });
+      }
+    });
 };
 module.exports.createUser = (req, res) => {
-    const {
-        name,
-        about,
-        avatar
-    } = req.body;
-    User.create({
-            name,
-            about,
-            avatar
-        })
-        .then((user) => res.send({
-            data: user
-        }))
-        .catch((err) => {
-            if (err instanceof mongoose.Error.ValidationError) {
-                res.status(ERROR_BAD_DATA).send({
-                    message: 'Переданы некорректные данные пользователя.'
-                });
-            } else {
-                res.status(ERROR_DEFAULT).send({
-                    message: 'Произошла ошибка'
-                });
-            }
+  const {
+    name,
+    about,
+    avatar,
+  } = req.body;
+  User.create({
+    name,
+    about,
+    avatar,
+  })
+    .then((user) => res.send({
+      data: user,
+    }))
+    .catch((err) => {
+      if (err instanceof mongoose.Error.ValidationError) {
+        res.status(ERROR_BAD_DATA).send({
+          message: 'Переданы некорректные данные пользователя.',
         });
+      } else {
+        res.status(ERROR_DEFAULT).send({
+          message: 'Произошла ошибка',
+        });
+      }
+    });
 };
 module.exports.updateUser = (req, res) => {
-    const { name, about } = req.body;
-    userDataUpdate(req, res, { name, about });
-  };
-  
-  module.exports.updateAvatar = (req, res) => {
-    const { avatar } = req.body;
-    userDataUpdate(req, res, { avatar });
-  };
+  const { name, about } = req.body;
+  userDataUpdate(req, res, { name, about });
+};
+
+module.exports.updateAvatar = (req, res) => {
+  const { avatar } = req.body;
+  userDataUpdate(req, res, { avatar });
+};
--- models/card.js
+++ models/card.js
@@ -1,30 +1,30 @@
 const mongoose = require('mongoose');
 
 const cardSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true,
-        minlength: 2,
-        maxlength: 30,
-    },
+  name: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 30,
+  },
 
-    link: {
-        type: String,
-        required: true,
-    },
-    owner: {
-        type: mongoose.ObjectId,
-        required: true,
-        ref: 'user',
-    },
-    likes: [{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'user',
-    }, ],
-    createdAt: {
-        type: Date,
-        default: Date.now,
-    },
+  link: {
+    type: String,
+    required: true,
+  },
+  owner: {
+    type: mongoose.ObjectId,
+    required: true,
+    ref: 'user',
+  },
+  likes: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'user',
+  }],
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
 module.exports = mongoose.model('card', cardSchema);
